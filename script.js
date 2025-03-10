032bc0dd49a951a3a066e51176666b49e36095ad
Therapy-Man <129146249+Therapy-Man@users.noreply.github.com>
2025-03-10 09:00:31 +0545

    initialized commit with website files

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+        document.querySelector(".splash").classList.add("hidden");
+    }, 4000); // Hide splash after 2 seconds
+});
